732616ca70ebcd55611c575ca478e1b4f92f7302
Kavitha <kavithamd.kavi@gmail.com>
2022-12-11 17:01:21 +0530

    bug: fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var outputDiv = document.querySelector(".output");
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(input) {
-  return (serverURL = "?" + "text" + input);
+  return serverURL + "?input=" + input;
 }
 
 function errorHandler(error) {
