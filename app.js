748f4d4af374fe9a6c079c04e636755fd678cb56
Kavitha <kavithamd.kavi@gmail.com>
2023-01-05 01:11:20 +0530

    minor fixes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var outputDiv = document.querySelector(".output");
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(input) {
-  return serverURL + "?input=" + input;
+  return serverURL + "?text=" + input;
 }
 
 function errorHandler(error) {
@@ -13,16 +13,18 @@ function errorHandler(error) {
   alert("something wrong with the server! try again after some time");
 }
 
-btnTranslate.addEventListener("click", clickHandler);
 
 function clickHandler() {
   var inputText = txtInput.value;
+  console.log(inputText);
 
   fetch(getTranslationURL(inputText))
-    .then((response) => response.json())
-    .then((json) => {
+    .then(response => response.json())
+    .then(json => {
       var translatedText = json.contents.translated;
       outputDiv.innerText = translatedText;
     })
-    .catch(errorHandler);
-}
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler);
